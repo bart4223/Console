7e1f221487bdcadaf29619744aad20891a907803
Nils <nilsgrimmer@gmx.de>
2017-08-23 07:28:22 +0200

    Initial commit

---
+++ src/Console/Main.java
@@ -0,0 +1,43 @@
+package Console;
+
+import Uniwork.Appl.NGApplication;
+import Uniwork.Appl.NGCustomApplicationModule;
+import Uniwork.Appl.NGVisualApplicationModule;
+
+import java.util.Iterator;
+
+public class Main extends NGApplication {
+
+    @Override
+    public void DoBeforeInitialize() {
+        super.DoBeforeInitialize();
+        NGVisualApplicationModule main = (NGVisualApplicationModule)addModule(MainApplicationModule.class, "Main");
+        main.setPrimaryStage(FPrimaryStage);
+    }
+
+    @Override
+    protected void registerObjectRequests() {
+        super.registerObjectRequests();
+        registerObjectRequest("TestConsole", this, "Test", "TestTheBest", "\"This is a test method.\"");
+    }
+
+    public Main() {
+        super();
+        FName = "Test-Console";
+        FDescription = "Test-Console is a project for testing the console";
+        FConfigurationFilename = "resources/config.acf";
+    }
+
+    public void TestTheBest() {
+        Iterator<NGCustomApplicationModule> itr = FModuleManager.getModules();
+        while (itr.hasNext()) {
+            NGVisualApplicationModule mod = (NGVisualApplicationModule)itr.next();
+            writeInfo(String.format("%s: %s", mod.getFullName(), mod.getStageCount()));
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
